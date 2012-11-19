1707477bf289b3a23ac3c39e13dfeaf60fdf21cb
rajpals <rajpals@1e.com>
2012-11-19 23:30:56 +0000

    Gherkin contains syntax for "But" not "Or"

--- Src/Parser/SyntaxParser.cs
+++ Src/Parser/SyntaxParser.cs
@@ -13,7 +13,7 @@ namespace GherkinSyntaxHighlighter.Parser
         private readonly ISyntaxParserObserver syntaxParserObserver;
         private readonly Regex regex = new Regex(@"(?<!__)([A-Z][a-z]+|(?<=[a-z])[A-Z]+)", RegexOptions.Compiled);
 
-        private readonly string[] gherkinLanguageKeywords = new[] { "Given", "When", "Then", "And", "Or" };
+        private readonly string[] gherkinLanguageKeywords = new[] { "Given", "When", "Then", "And", "But" };
  
         public SyntaxParser(ISyntaxParserObserver syntaxParserObserver)
         {
